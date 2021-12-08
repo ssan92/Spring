e0a039ee1bcb0db848f75bf5f2697e2f554b97a1
Santiago <ssantiago192@gmail.com>
2021-12-08 16:23:31 -0500

    Delete ACME directory

--- ACME/src/main/java/ec/com/ioet/ACME/constantes/AcmeConstante.java
+++
@@ -1,7 +0,0 @@
-package ec.com.ioet.ACME.constantes;
-
-public final class AcmeConstante {
-	public static final String DDMMYYYY="dd/MM/yyyy";
-	public static final String YYYYMMDD="yyyy-MM-dd";
-	public static final String YYYYMMDDHH24MISS="yyyy-MM-dd HH:mm:ss";
-}
